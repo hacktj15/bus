e4ea4f3141976f1d10363216784498bab4106d10
James Woglom <j@wogloms.net>
2015-05-16 17:13:08 -0400

    add admin bindings

--- bus/busapp/admin.py
+++ bus/busapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register([
+    models.County,
+    models.Bus,
+    models.Coordinate,
+    models.Slot,
+    models.BusInstance
+])
