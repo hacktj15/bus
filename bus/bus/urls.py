eac099ee3fc5bd24d22abf3ccf1a676493ff135c
James Woglom <j@wogloms.net>
2015-05-16 19:52:16 -0400

    urls py

--- bus/bus/urls.py
+++ bus/bus/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', 'busapp.views.display_view', name='display_view'),
+    url(r'^$', 'busapp.views.display_view', name='index_view'),
+    url(r'^display$', 'busapp.views.display_view', name='display_view'),
     url(r'^login$', 'busapp.views.login_view', name='login_view'),
     url(r'^modify$', 'busapp.views.modify_view', name='modify_view')
 )
