ddef54e5ca0f2547c73b834ebb0b690d5c114e78
James Woglom <j@wogloms.net>
2015-05-17 08:50:12 -0400

    css move and is authenticated check

--- bus/busapp/context_processors.py
+++ bus/busapp/context_processors.py
@@ -2,10 +2,12 @@ from .models import BusUser
 from django.contrib.auth.models import User
 def is_admin(request):
     """Add whether the user is an admin."""
-    try:
-        bususer = BusUser.objects.get(user__id=request.user.id)
-    except BusUser.DoesNotExist:
-        bususer = BusUser.objects.create(user=request.user)
-    admin = bususer.admin
-
+    if request.user and request.user.is_authenticated():
+        try:
+            bususer = BusUser.objects.get(user__id=request.user.id)
+        except BusUser.DoesNotExist:
+            bususer = BusUser.objects.create(user=request.user)
+        admin = bususer.admin
+    else:
+        admin = False
     return {"is_admin": admin}
