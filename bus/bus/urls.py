74955516a12594143663107239861c3c36cdb822
James Woglom <j@wogloms.net>
2015-05-17 14:28:21 -0400

    favicons and icons

--- bus/bus/urls.py
+++ bus/bus/urls.py
@@ -1,5 +1,7 @@
+from django.views.generic.base import RedirectView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from . import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
 
     url(r'^$', 'busapp.views.index_view', name='index_view'),
     url(r'^display$', 'busapp.views.display_view', name='display_view'),
