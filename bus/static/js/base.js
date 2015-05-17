436da87f1dc3fdb8a4175bc4e7ee6080b171ab52
James Woglom <j@wogloms.net>
2015-05-17 02:18:29 -0400

    saveDrop

--- bus/static/js/base.js
+++ bus/static/js/base.js
@@ -1,1 +1,13 @@
-/* */
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    crossDomain: false,
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type)) {
+            xhr.setRequestHeader("X-CSRFToken", $.cookie("csrftoken"));
+        }
+    }
+});
